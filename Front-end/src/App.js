b07bc6315cbe9a5710ef8100a8ceb237147374f3
YellowYoCat <125392679+YellowYoCat@users.noreply.github.com>
2024-06-27 17:57:02 -0600

    Nav
    creating the Navbar, and the general setup of the pages.

--- Front-end/src/App.js
+++ Front-end/src/App.js
@@ -1,9 +1,28 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {Route, Routes } from 'react-router-dom';
+import Home from './Componets/Pages/Home';
+import Contact from './Componets/Pages/Contact';
+import Profile from './Componets/Pages/Profile';
+import Shop from './Componets/Pages/Shop';
+import Social from './Componets/Pages/Social';
+import Cart from './Componets/Pages/Cart';
+import Navbar from './Componets/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/shop' element={<Shop/>}/>
+        <Route path='/social' element={<Social/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/profile' element={<Profile/>}/>
+      </Routes>
+      </Navbar>
+      
       
     </div>
   );
