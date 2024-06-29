6826aab9e2cf9a32aee4785a3fb571e8b1338d53
YellowYoCat <125392679+YellowYoCat@users.noreply.github.com>
2024-06-29 14:25:56 -0600

    trying to do commerce.js
    trying to figure out how commerce.js works. I've never used it before.

--- Front-end/src/Componets/Pages/Shop.js
+++ Front-end/src/Componets/Pages/Shop.js
@@ -1,11 +1,46 @@
 import React from 'react'
 import Navbar from './Nav/Navbar';
+import Commerce from '@chec/commerce.js'
+import { commerce } from '@chec/commerce.js'
+import { useState, useEffect } from 'react';
+import './Shop.css'
 
 const Shop = () => {
+  const commerce = new Commerce('pk_576186827786196c6606cd0287e2ae43938873da51144')
+  const [products, setProducts] = useState([])
+
+    useEffect(() => {
+        commerce.products.list()
+          .then(res => {
+            setProducts(res.data)
+          })
+          .catch(err => console.log(err))
+    },[])
+    
+    const fetchProducts = () => {
+      commerce.products.list().then((products) => {
+        setProducts(products.data);
+      }).catch((error) => {
+        console.log('There was an error fetching the products', error)
+      });
+    }
+
   return (
     <div>
-       <Navbar />
+      <div>
+        <Navbar />
+      </div>
+      <div className='banner'>
+        <h1 className='AP'>ALL PRODUCTS</h1>
+
+      
+      </div>
+      <div>
+        
+      
+      </div>
     </div>
+
   )
 }
 
