17862c55c426badf8913bbb6f6aef77f77d987ec
YellowYoCat <125392679+YellowYoCat@users.noreply.github.com>
2024-07-03 14:02:03 -0600

    ah

--- Front-end/src/App.js
+++ Front-end/src/App.js
@@ -14,3 +14,38 @@ const App = () => {
 };
 
 export default App;
+
+
+// import React, { useState } from 'react';
+// import ProductDetail from './ProductDetail';
+
+// const App = () => {
+//   const [cart, setCart] = useState([]);
+//   const product = {
+//     image: 'image-url.jpg',
+//     title: 'Product Title',
+//     description: 'Product Description',
+//     price: 29.99
+//   };
+
+//   const addToCart = (product) => {
+//     setCart([...cart, product]);
+//     alert(`${product.title} has been added to your cart!`);
+//   };
+
+//   const closeProductDetail = () => {
+//     // Logic to close the product detail view
+//   };
+
+//   return (
+//     <div>
+//       <ProductDetail 
+//         product={product} 
+//         closeClicked={closeProductDetail} 
+//         addToCartClicked={addToCart} 
+//       />
+//     </div>
+//   );
+// };
+
+// export default App;
--- Front-end/src/Componets/Product/ProductDetail.js
+++ Front-end/src/Componets/Product/ProductDetail.js
@@ -1,16 +1,39 @@
+// import React from 'react'
+// import './ProductDetail.css';
+
+// const ProductDetail = ({product,  closeClicked}) => {
+//     return (
+//         <div className="product-item">
+//         <img src={product.image} alt={product.title} />
+//         <h2>{product.title}</h2>
+//         <p>{product.description}</p>
+//         <p><strong>${product.price}</strong></p>
+//         <button onClick={() => (closeClicked())}>close</button>
+//       </div>
+//     )
+// }
+
+// export default ProductDetail
+
 import React from 'react'
 import './ProductDetail.css';
 
-const ProductDetail = ({product,  closeClicked}) => {
-    return (
-        <div className="product-item">
+
+const ProductDetail = ({ product, closeClicked, addToCartClicked }) => {
+  return (
+    <div className='container'>
+      <div className="product-item">
         <img src={product.image} alt={product.title} />
         <h2>{product.title}</h2>
         <p>{product.description}</p>
         <p><strong>${product.price}</strong></p>
-        <button onClick={() => (closeClicked())}>close</button>
+        <button onClick={() => (closeClicked())}>Close</button>
+        <button className="add-to-cart" onClick={() => (addToCartClicked(product))}>Add to Cart</button>
       </div>
-    )
+
+    </div>
+
+  )
 }
 
-export default ProductDetail
+export default ProductDetail;
