30458e179ef5f84b41882f519e963fc94f6eace2
YellowYoCat <125392679+YellowYoCat@users.noreply.github.com>
2024-07-07 15:59:40 -0600

    social

--- Front-end/src/Componets/Pages/Social.js
+++ Front-end/src/Componets/Pages/Social.js
@@ -1,12 +1,32 @@
-import React from 'react'
+import React from 'react';
 import Navbar from './Nav/Navbar';
+import './Social.css'; // Import the CSS file
 
 const Social = () => {
   return (
     <div>
-       <Navbar />
+      <Navbar />
+      <div className="social-container">
+        <h1 className="social-title">SOCIALS</h1>
+        <div className="phone">
+          <div className="icons">
+          <a href="https://www.twitch.tv/dokibird" target="_blank" rel="noopener noreferrer">
+              <img src="twitch.png" alt="Twitch" className="icon" />
+            </a>
+            <a href="https://www.youtube.com/@Dokibird" target="_blank" rel="noopener noreferrer">
+              <img src="YT.png" alt="YouTube" className="icon" />
+            </a>
+            <a href="https://www.tiktok.com/@dokibird" target="_blank" rel="noopener noreferrer">
+              <img src="TT.png" alt="TikTok" className="icon" />
+            </a>
+            <a href="https://x.com/dokibird" target="_blank" rel="noopener noreferrer">
+              <img src="x.png" alt="X" className="icon" />
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Social
+export default Social;
