55283274f5927a7ea89ebfe689b342d5d531e85b
YellowYoCat <125392679+YellowYoCat@users.noreply.github.com>
2024-07-10 20:30:38 -0600

    starting db
    starting to get the database connection setup

--- Back-end/Back-end.js
+++ Back-end/Back-end.js
@@ -1,1 +1,18 @@
 console.log("does this work?")
+const { MongoClient } = require('mongodb');
+const uri = "mongodb+srv://Johanna:Jj3380@rest.4gkziko.mongodb.net/?retryWrites=true&w=majority&appName=rest";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        console.log("Connected to MongoDB Atlas");
+        const database = client.db('Ecom');
+        const collection = database.collection('Data');
+        
+    } finally {
+        await client.close();
+    }
+}
+
+run().catch(console.dir);
